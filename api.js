68a50624e69ff81516ba165fc3d212b7b6597fce
Casraw <casraw70@googlemail.com>
2024-05-04 13:57:52 +0200

    Update: try to fix the problems

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ const port = 8081;
 let timer = null;
 
 app.get('/start-pod', (req, res) => {
-    const podName = process.env.OLLAMA_POD_NAME;;
+    const podName = process.env.OLLAMA_POD_NAME;
     const cmd = `sudo /usr/local/bin/runpodctl start pod ${podName}`;
     
     exec(cmd, (error, stdout, stderr) => {
@@ -26,7 +26,7 @@ app.get('/start-pod', (req, res) => {
 });
 
 app.get('/stop-pod', (req, res) => {
-    const podName = process.env.OLLAMA_POD_NAME;;
+    const podName = process.env.OLLAMA_POD_NAME;
     const cmd = `sudo /usr/local/bin/runpodctl stop pod ${podName}`;
 
     exec(cmd, (error, stdout, stderr) => {
@@ -48,43 +48,40 @@ app.get('/install-pod', (req, res) => {
             return res.status(500).send(`Error installing pod: ${installStderr}`);
         }
 
-        // Pod wurde erfolgreich installiert, hole die Pod-ID des "ollama-node" Pods
         const getPodIdCmd = `sudo /usr/local/bin/runpodctl get pod | awk '/ollama-node/ {print $1; exit}'`;
         exec(getPodIdCmd, (getPodIdError, ollamaPodId, getPodIdStderr) => {
             if (getPodIdError) {
                 console.error(`Get Pod ID error: ${getPodIdStderr}`);
                 return res.status(500).send(`Error retrieving ollama-node pod ID: ${getPodIdStderr}`);
             }
-        const podId = ollamaPodId.trim();
+            const podId = ollamaPodId.trim();
             
-        // Pod wurde erfolgreich installiert, hole die Pod-ID eines "open-webui"-Pods
-        const getIdCmd = `sudo /usr/local/bin/runpodctl get pod | awk '/open-webui/ {print $1; exit}'`;
-        exec(getIdCmd, (getIdError, webuiPodId, getIdStderr) => {
-            if (getIdError) {
-                console.error(`Get ID error: ${getIdStderr}`);
-                return res.status(500).send(`Error retrieving pod ID: ${getIdStderr}`);
-            }
-            // Pod-ID für open-webui erhalten, führe das Curl-Kommando aus
-            const apiKey = process.env.API_KEY;
-            const curlCmd = `curl --request POST \
-                --header 'content-type: application/json' \
-                --url 'https://api.runpod.io/graphql?api_key=${apiKey}' \
-                --data '{"query": "mutation { podEditJob(input: { podId: "${webuiPodId.trim()}", containerDiskInGb: 5, dockerArgs: \"\", env: [ { key: \"OLLAMA_BASE_URL\", value: \"https://${podId}-11434.proxy.runpod.net\" }, { key: \"API_KEY\", value: \"{{ RUNPOD_SECRET_runpod-api }}\" }, { key: \"OLLAMA_POD_NAME\", value: \"${podId}\" } ], imageName: \"casraw/open-webui-runpod-integration:latest\", ports: \"8080/http,8081/http\", volumeInGb: 0, volumeMountPath: \"/app/backend/data\" }) { containerDiskInGb costPerHr desiredStatus dockerArgs env gpuCount id imageName lastStatusChange locked machineId memoryInGb name networkVolume { dataCenterId id name size } podType ports templateId uptimeSeconds vcpuCount volumeInGb volumeMountPath } }"'`;
-
-            exec(curlCmd, (curlError, curlStdout, curlStderr) => {
-                if (curlError) {
-                    console.error(`Curl error: ${curlStderr}`);
-                    return res.status(500).send(`Error during API call: ${curlStderr}`);
+            const getIdCmd = `sudo /usr/local/bin/runpodctl get pod | awk '/open-webui/ {print $1; exit}'`;
+            exec(getIdCmd, (getIdError, webuiPodId, getIdStderr) => {
+                if (getIdError) {
+                    console.error(`Get ID error: ${getIdStderr}`);
+                    return res.status(500).send(`Error retrieving pod ID: ${getIdStderr}`);
                 }
-                res.send('Pod installed and configured successfully!');
+                const apiKey = process.env.API_KEY;
+                const curlCmd = `curl --request POST \
+                    --header 'content-type: application/json' \
+                    --url 'https://api.runpod.io/graphql?api_key=${apiKey}' \
+                    --data '{"query": "mutation { podEditJob(input: { podId: "${webuiPodId.trim()}", containerDiskInGb: 5, dockerArgs: \"\", env: [ { key: \"OLLAMA_BASE_URL\", value: \"https://${podId}-11434.proxy.runpod.net\" }, { key: \"API_KEY\", value: \"${apiKey}\" }, { key: \"OLLAMA_POD_NAME\", value: \"${podId}\" } ], imageName: \"casraw/open-webui-runpod-integration:latest\", ports: \"8080/http,8081/http\", volumeInGb: 0, volumeMountPath: \"/app/backend/data\" }) { containerDiskInGb costPerHr desiredStatus dockerArgs env gpuCount id imageName lastStatusChange locked machineId memoryInGb name networkVolume { dataCenterId id name size } podType ports templateId uptimeSeconds vcpuCount volumeInGb volumeMountPath } }"'`;
+
+                exec(curlCmd, (curlError, curlStdout, curlStderr) => {
+                    if (curlError) {
+                        console.error(`Curl error: ${curlStderr}`);
+                        return res.status(500).send(`Error during API call: ${curlStderr}`);
+                    }
+                    res.send('Pod installed and configured successfully!');
+                });
             });
         });
     });
 });
 
-
 app.get('/kill-pod', (req, res) => {
-    const podName = process.env.OLLAMA_POD_NAME;;
+    const podName = process.env.OLLAMA_POD_NAME;
     const cmd = `sudo /usr/local/bin/runpodctl remove pods ollama-node`;
 
     exec(cmd, (error, stdout, stderr) => {
